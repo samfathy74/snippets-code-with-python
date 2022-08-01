925ee5bc5bdf774445e350abfbeb0d65d8950cb9
Salim Fathy <61642486+samfathy74@users.noreply.github.com>
2022-08-01 11:58:31 +0200

    Create plot_deep_learning_layers.py

---
+++ plot_deep_learning_layers.py
@@ -0,0 +1,7 @@
+# TUTORIAL
+# https://analyticsindiamag.com/how-to-visualize-deep-learning-models-using-visualkeras/
+!pip install visualkeras
+
+import visualkeras
+
+visualkeras.layered_view(MODEL, legend=True, background_fill='white')
