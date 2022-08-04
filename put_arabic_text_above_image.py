6a6c848d218e50a10c8678d58a5ef01ce9006d9b
Salim Fathy <61642486+samfathy74@users.noreply.github.com>
2022-08-04 08:07:46 +0200

    Update put_arabic_text_above_image.py

--- put_arabic_text_above_image.py
+++ put_arabic_text_above_image.py
@@ -1,7 +1,14 @@
+import os
+os.system("git clone https://github.com/gasharper/linux-fonts.git")
+os.system("pip install arabic_reshaper")
+os.system("pip install python-bidi")
+import cv2 
+import arabic_reshaper
+from bidi.algorithm import get_display
+import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+
 def put_ar_text(img, text, cor=(0,0), font_size=32, fontpath="./linux-fonts/arial.ttf", fill_color=(255,255,0), windows=False):
-  os.system("git clone https://github.com/gasharper/linux-fonts.git")
-  os.system("pip install arabic_reshaper")
-  os.system("pip install python-bidi")
   font = ImageFont.truetype(fontpath, font_size)
   img_pil = Image.fromarray(img)
   draw = ImageDraw.Draw(img_pil)
